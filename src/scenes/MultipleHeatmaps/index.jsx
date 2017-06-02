93e52176e0b0b5c400e578500f0e18277f73d954
Luciano Di Pasquale <yolucianodipasquale@gmail.com>
2017-06-02 15:23:37 -0300

    Remove hardcoded dimensions

--- src/scenes/MultipleHeatmaps/index.jsx
+++ src/scenes/MultipleHeatmaps/index.jsx
@@ -39,50 +39,19 @@ export default class MultipleHeatmaps extends React.Component {
   }
 
   render() {
-    const adIdDimension = {
-      3974313: { w: 320, h: 480 },
-      3974316: { w: 320, h: 480 },
-      3974451: { w: 320, h: 480 },
-      3974454: { w: 320, h: 480 },
-      3974452: { w: 300, h: 250 },
-      3974453: { w: 320, h: 50 },
-      3974456: { w: 320, h: 50 },
-      3974457: { w: 320, h: 480 },
-      3974459: { w: 320, h: 50 },
-      3974460: { w: 320, h: 480 },
-      3974462: { w: 320, h: 50 },
-      3974463: { w: 320, h: 480 },
-      3974465: { w: 320, h: 50 },
-      3974466: { w: 320, h: 480 },
-      3974468: { w: 320, h: 50 },
-      3974557: { w: 320, h: 480 },
-      3974563: { w: 320, h: 480 },
-      3974573: { w: 320, h: 480 },
-      3974582: { w: 320, h: 480 },
-    };
-
-    const getAdDimension = id => adIdDimension[id];
-
-    let adDimension;
-
     return (
       <div>
-        {this.ads.map(ad => {
-          adDimension = getAdDimension(ad.id);
-          console.log(ad.id);
-
-          return (
-            <div style={{display: 'inline-block'}}>
-              <Heatmap
-                key={ad.id}
-                id={ad.id}
-                points={ad.points}
-                width={adDimension.w}
-                height={adDimension.h}
-              />
-            </div>
-           )
-        })}
+        {this.ads.map(ad => (
+          <div style={{display: 'inline-block'}}>
+            <Heatmap
+              key={ad.id}
+              id={ad.id}
+              points={ad.points}
+              width={adDimension.w}
+              height={adDimension.h}
+            />
+          </div>
+        ))}
       </div>
     );
   }
